484367d9292a2d09c2abd01dc7c46ce5fe06cba5
travis-wentz <wentz.travis@gmail.com>
2016-04-18 23:13:51 -0600

    packet handler

--- src/Lab5/Ids.java
+++ src/Lab5/Ids.java
@@ -1,7 +1,8 @@
 package Lab5;
-//Dustin is here
+//Travis is here
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.Date;
@@ -10,22 +11,51 @@ import java.util.Scanner;
 import org.jnetpcap.Pcap;
 import org.jnetpcap.packet.PcapPacket;
 import org.jnetpcap.packet.PcapPacketHandler;
+import org.jnetpcap.packet.format.FormatUtils;
+import org.jnetpcap.packet.format.XmlFormatter;
+import org.jnetpcap.protocol.network.Ip4;
+import org.jnetpcap.protocol.network.Ip6;
 
 public class Ids {
+
+	private static int x = 1; //for testing packet handler loop
 	
+	/*
+	 * NOTE: most of the code in the following method was taken from the open-source
+	 * 		tutorials by jnetpcap
+	 * 		http://jnetpcap.com/?q=userguide
+	 */
 	private static void readPcapFile(String pcapFileName){
 		StringBuilder err = new StringBuilder();
 		Pcap pcap = Pcap.openOffline(pcapFileName, err);
 		
 		if(pcap == null){
 			System.out.println("There was an error reading your trace file. "
-					+ "Please make sure it is in the proper directory and spelled correctly.");
+					+ "Please make sure it is in the project folder and is spelled correctly.");
 			return;
 		}
+        
+		PcapPacketHandler<String> handler = new PcapPacketHandler<String>() {
+			public void nextPacket(PcapPacket packet, String user) {
+				System.out.println(x + " " + "size of packet is=" + packet.size());
+				x++;
+				
+//				//print out all the packet info
+//				XmlFormatter out = new XmlFormatter(System.out);  
+//				try {
+//					out.format(packet);
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+			}
+		};
 		
-		//TODO figure out how to read the packets
+		pcap.loop(-1, handler, "hi"); //the "-1" value makes it run infinite times (to the end of the file)
 		
 		//TODO regexs to match policies
+        
+        pcap.close();
 	}
 	
 	//this should take in a packet as an argument
@@ -62,7 +92,7 @@ public class Ids {
 //			readPcapFile(input);
 //		}
 		
-		readPcapFile("trace1.pcap");
+		readPcapFile("trace5.pcap");
 	}
 
 }
