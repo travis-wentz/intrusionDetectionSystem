b2efef8cdd0b2170981abbd7ec70c7c9d780b192
travis-wentz <wentz.travis@gmail.com>
2016-04-19 22:56:35 -0600

    done

---
+++ README.txt
@@ -0,0 +1,2 @@
+Run this with 2 arguments: 1) policy file 2) trace file
+Policy file should be a .txt file with all policies in it and the trace file should be a .pcap file.
--- src/Lab5/Ids.java
+++ src/Lab5/Ids.java
@@ -21,6 +21,7 @@ import org.jnetpcap.packet.format.FormatUtils;
 import org.jnetpcap.packet.format.XmlFormatter;
 import org.jnetpcap.protocol.network.Ip4;
 import org.jnetpcap.protocol.tcpip.Tcp;
+import org.jnetpcap.protocol.tcpip.Udp;
 
 /**
  * 
@@ -31,11 +32,12 @@ public class Ids {
 
     private static int x = 1; //for packet handler loop
     private static String host = null;
-    private static LinkedList<Policy> policies = new LinkedList();
+    private static LinkedList<Policy> policies = new LinkedList<Policy>();
+    private static String memDump = "";
     //private static final String ipRegex = "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
     
     
-    private static void readPolicyFile(String policyFileName) throws IOException{
+     private static void readPolicyFile(String policyFileName) throws IOException{
         Pattern hostPattern1 = Pattern.compile("host=(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
         Pattern namePattern = Pattern.compile("name=(.)+$");
         Pattern typePattern = Pattern.compile("stateful|stateless");
@@ -150,87 +152,107 @@ public class Ids {
         PcapPacketHandler<String> handler = new PcapPacketHandler<String>() {
             final Ip4 ip4 = new Ip4();
             final Tcp tcp = new Tcp();
+            final Udp udp = new Udp();
             final Payload payload = new Payload();
-            Pattern pattern = Pattern.compile("Now I own your computer");
-            Matcher match;
-            String test = "";
-            String regex = "[^((0-9a-zA-Z){2}' '.)] ";
+            
+//            String test = "";
+//            String regex = "[^((0-9a-zA-Z){2}' '.)] ";
             
 
             public void nextPacket(PcapPacket packet, String user) {
+            	packet.getHeader(tcp);
                 String currPayload = "";
-                if (packet.hasHeader(Ip4.ID)) {
-                    packet.getHeader(ip4);
-
+                packet.getHeader(ip4);
+                if (packet.hasHeader(Ip4.ID) && (FormatUtils.ip(ip4.destination()).equals(host)) && packet.hasHeader(Payload.ID)) {
+                    
+                    
                     if(packet.hasHeader(payload)){                    
-                        currPayload = payload.toHexdump();
-                        byte[] byteArray = payload.getByteArray(0, payload.getLength());
-                        try{
-                            currPayload = new String(byteArray, "UTF-8");
-                        }catch(UnsupportedEncodingException e){
-                            System.out.println("Failed");
-                            e.printStackTrace();
-                        }
-                        match = pattern.matcher(currPayload);
-                  //      System.out.println("\n The payload info is :\n " + currPayload + "\n");
-                        if(match.find()) {
-                          System.out.println("A match was found for '"+match.group()+"'");
-                          String currSource = FormatUtils.ip(ip4.source());
-                          System.out.println("The source IP is : " + currSource + "\n");
-                        }
+                      currPayload = payload.toHexdump();
+                      byte[] byteArray = payload.getByteArray(0, payload.getLength());
+                      try{
+                          currPayload = new String(byteArray, "UTF-8");
+                          memDump += currPayload;
+                      }catch(UnsupportedEncodingException e){
+                          System.out.println("Failed");
+                          e.printStackTrace();
+                      }
+                    }
+                    
+                    for(int i = 0; i < policies.size(); i++){
+//                    	Pattern pattern = Pattern.compile(policies.get(i).getToHost().get(0));
+//                        Matcher match;
+//                        match = pattern.matcher(currPayload);
+//                        if(match.find()) {
+                        	//policies.get(i).printPolicy();
+//                    	String foobar = memDump;
+//                		if(policies.get(i).getType().equals("stateless")){
+//                			foobar = currPayload;
+//                		}
+            			if(checkMsg(policies.get(i).getToHost(),policies.get(i).getFromHost(),currPayload)){
+                        	if(policies.get(i).getAttacker().equals("any") || policies.get(i).getAttacker().equals(FormatUtils.ip(ip4.source()))){
+                        		int source = 0;
+                        		int destination = 0;
+                        		if(packet.hasHeader(tcp)){
+                        			source = tcp.source();
+                        			destination = tcp.destination();
+                        		}
+                        		else if(packet.hasHeader(udp)){
+                        			source = udp.source();
+                        			destination = udp.destination();
+                        		}
+                        		if(policies.get(i).getAttackerPort().equals("any") || (Integer.parseInt(policies.get(i).getAttackerPort())) == source){
+                        			if(policies.get(i).getHostPort().equals("any") || (Integer.parseInt(policies.get(i).getHostPort())) == destination){
+	                        			System.out.println("A match was found for '" + policies.get(i).getName() +"'");
+		                                String currSource = FormatUtils.ip(ip4.source());
+		                                System.out.println("The attacker IP is : " + currSource + "\n");
+                        			}
+                        		}
+                        	}
+            			}
                     }
                 }
-//				System.out.println(x + " " + "size of packet is=" + packet.size());
-//				x++;
-
-                //print out all the packet info
-//                XmlFormatter out = new XmlFormatter(System.out);
-//                try {
-//
-//                    out.format(packet);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
             }
         };
+        
         pcap.loop(-1, handler, "hi"); //the "-1" value makes it run infinite times (to the end of the file)
 
         //TODO regexs to match policies
         pcap.close();
     }
+    
+    private static boolean checkMsg(LinkedList<String> toHost, LinkedList<String> fromHost, String payload){
+    	
+    	for(int i = 0; i < toHost.size(); i++){
+    		Pattern pattern = Pattern.compile(toHost.get(i));
+            Matcher match;
+            match = pattern.matcher(payload);
+            if(match.find()) {
+            	//System.out.println(match.group());
+            	return true;
+            }
+    	}
+    	for(int i = 0; i < toHost.size(); i++){
+    		Pattern pattern = Pattern.compile(toHost.get(i));
+            Matcher match;
+            match = pattern.matcher(payload);
+            if(match.find()) {
+            	//System.out.println(match.group());
+            	return true;
+            }
+    	}
+    	return false;
+    }
 
     public static void main(String[] args) throws IOException {
-//		Scanner in = new Scanner(System.in);
-//		String traceName = null;
-//		String policyName = null;
-//		File policyFile;
-//		File traceFile;
-//		
-//		System.out.println("Please enter the name of the policy file:");
-//		policyName = in.nextLine();
-//		policyFile = new File(policyName);
-//		if(!policyFile.exists()){
-//			policyName += ".txt";
-//			policyFile = new File(policyName);
-//			if(!policyFile.exists()){
-//				System.out.println("Policies file not found. Please make sure it is in the project folder for lab 5.");
-//				in.close();
-//				return;
-//			}
-//		}
-//		
-//		System.out.println("Please enter the name of the trace file:");
-//		traceName = in.nextLine();
-//		in.close();
-//		traceFile = new File(traceName);
-//		if(!traceFile.exists()){
-//			System.out.println("Trace file not found. Please make sure it is in the project folder for lab 5.");
-//		}else{
-//			readPolicyFile(policyName, traceName);
-//		}
-
-    	readPolicyFile("policies.txt");
-        readPcapFile("trace1.pcap");
+		String traceName = "trace1.pcap";
+		String policyName = "policies.txt";
+ 
+		if(1<args.length){
+			traceName = args[1];
+			policyName = args[0];
+		}
+    	readPolicyFile(policyName);
+        readPcapFile(traceName);
     }
 
 }
